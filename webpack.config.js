ef180f1386db356f4ebda54daf3b2482c612ecc0
Blake Goral <blakegoral@gmail.com>
2020-10-05 21:21:50 -0500

    set up basic template

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,8 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+    .enablePostCssLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
