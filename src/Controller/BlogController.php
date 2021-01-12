e30f55dfe236e8da2a4e2286a2dd5feb3b1e5196
Blake Goral <blakegoral@gmail.com>
2021-01-11 22:02:57 -0600

    add new post page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Form\BlogPostFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +17,17 @@ class BlogController extends AbstractController
     public function homepage(): Response
     {
         return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/post/new")
+     */
+    public function new()
+    {
+        $form = $this->createForm(BlogPostFormType::class);
+
+        return $this->render('new.html.twig', [
+            'blogForm' => $form->createView()
+        ]);
     }
 }
